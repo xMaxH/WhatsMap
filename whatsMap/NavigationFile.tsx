a683db37c8f25b5bb6ba995055cbc491f0e18ae7
max høigilt <maxh@uia.no>
2024-04-08 23:13:29 +0200

    Endet slik at man ikke ser login når man er logget in

--- whatsMap/NavigationFile.tsx
+++ whatsMap/NavigationFile.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { onAuthStateChanged } from 'firebase/auth'; // Import Firebase authentication functions here
+import {auth} from "./Screens/HomeMap"
 // Screens
 import HomeMap from "./Screens/HomeMap";
 import Profile from "./Screens/Profile";
@@ -11,6 +13,15 @@ import Register from "./Screens/Register";
 const Stack = createNativeStackNavigator();
 
 export function NavigationFile() {
+    const [user, setUser] = useState(null); // State to hold the user's authentication state
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+        return unsubscribe; // Cleanup function
+    }, []);
+
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName={"Home"}>
@@ -25,11 +36,13 @@ export function NavigationFile() {
                                     title="Profile"
                                     color="#000" // Adjust the color as per your theme
                                 />
-                                <Button
-                                    onPress={() => navigation.navigate('Login')}
-                                    title="Login"
-                                    color="#000" // Adjust the color as per your theme
-                                />
+                                {!user && (
+                                    <Button
+                                        onPress={() => navigation.navigate('Login')}
+                                        title="Login"
+                                        color="#000" // Adjust the color as per your theme
+                                    />
+                                )}
                             </View>
                         )
                     })}
