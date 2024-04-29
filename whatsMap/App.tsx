70f40751be06dbc4312f993fc78f6e23ed41e716
Thomar002 <132343301+Thomar002@users.noreply.github.com>
2024-04-29 10:32:31 +0200

    Update App.tsx

--- whatsMap/App.tsx
+++ whatsMap/App.tsx
@@ -1,74 +1,120 @@
-import React, { useEffect, useState } from 'react';
+
 import Login from "./Screens/Login";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from "@react-navigation/native";
 import Profile from "./Screens/Profile";
-import HomeScreen, { auth } from "./Screens/HomeMap";
-import Register from "./Screens/Register";
-import AboutUs from "./Screens/AboutUs";
-import Drawer from "./Screens/Drawer";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { onAuthStateChanged } from "firebase/auth";
+import Drawer from "./Screens/Drawer";
+import {useEffect, useState} from "react";
+import {onAuthStateChanged} from "firebase/auth";
+import HomeScreen, {auth} from "./Screens/HomeMap"
+import Register from "./Screens/Register";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AboutUs from "./Screens/AboutUs";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-const MainTabScreen = ({ user }) => {
-    return (
-        <Tab.Navigator initialRouteName="Home">
-            {!user ? (
-                <Tab.Screen name="Login" component={Login}
-                            options={{
-                                tabBarLabel: 'Login',
-                                tabBarIcon: ({ color }) => <Icon name="login" size={25} color={color} />,
-                            }}
-                />
-            ) : (
-                <>
-                    <Tab.Screen name="Home" component={HomeScreen}
-                                options={{
-                                    tabBarLabel: 'Home',
-                                    headerShown: false,
-                                    tabBarIcon: ({ color }) => <Icon name="home-map-marker" size={25} color={color} />,
-                                }}
-                    />
-                    <Tab.Screen name="Profile" component={Profile}
-                                options={{
-                                    tabBarLabel: 'Profile',
-                                    tabBarIcon: ({ color }) => <Icon name="account" size={25} color={color} />,
-                                }}
-                    />
-                    <Tab.Screen name="Explore" component={Drawer}
-                                options={{
-                                    tabBarLabel: 'Explore',
-                                    tabBarIcon: ({ color }) => <Icon name="monitor-screenshot" size={25} color={color} />,
-                                }}
-                    />
-                </>
-            )}
-        </Tab.Navigator>
-    );
-};
+const MainTabScreen = () => (
+    <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+            name="Login"
+            component={Login}
+            options={{
+                tabBarLabel: 'Login',
+                tabBarIcon: ({ color }) => <Icon name="login" size={25} color={color} />,
+            }}
+        />
+        <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+                tabBarLabel: 'Home',
+                headerShown: false,
+                tabBarIcon: ({ color }) => <Icon name="home-map-marker" size={25} color={color} />,
+            }}
+
+        />
+        <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color }) => <Icon name="account" size={25} color={color} />,
+            }}
+        />
+        <Tab.Screen
+            name="Explore"
+            component={Drawer}
+            options={{
+                tabBarLabel: 'Explore',
+                tabBarIcon: ({ color }) => <Icon name="monitor-screenshot" size={25} color={color} />,
+            }}
+        />
+    </Tab.Navigator>
+);
 
 export default function App() {
     const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+    // Checks if user is logged in
+    useEffect(() =>
+        onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-        });
-        return unsubscribe;
-    }, []);
-
+        }), []);
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Main">
-                <Stack.Screen name="Main" options={{ headerShown: false }}>
-                    {() => <MainTabScreen user={user} />}
-                </Stack.Screen>
+            <Tab.Navigator initialRouteName={'Home'}>
+                {!user && (
+                    <Tab.Screen name="Login" component={Login}
+                                options={{
+                                    tabBarLabel: 'Login',
+                                    tabBarIcon: ({ color }) => (
+                                        <Icon name="login" size={25} />
+                                    )
+                                }}
+                    />
+                )}
+                <Tab.Screen name="Home" component={HomeScreen}
+                            options={{
+                                tabBarLabel: 'Home',
+                                tabBarIcon: ({ color }) => (
+                                    <Icon name="home-map-marker" size={25} />
+                                ),
+                                headerPressColor: 'red',
+                                headerTransparent: true,
+                                headerPressOpacity: 100,
+                                headerStatusBarHeight: 12,
+                                headerTitleAlign: 'center',
+                                headerTintColor: 'cyan',
+                            }}
+                />
+                {user && (
+                    <Tab.Screen name="Profile" component={Profile}
+                                options={{
+                                    tabBarLabel: 'Profile',
+                                    tabBarIcon: ({ color }) => (
+                                        <Icon name="account" size={25} />
+                                    )
+                                }}
+                    />
+                )}
+                <Tab.Screen name="Explore" component={Drawer}
+                            options={{
+                                tabBarLabel: 'Explore',
+                                tabBarIcon: ({ color }) => (
+                                    <Icon name="monitor-screenshot" size={25} />
+                                )
+                            }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+            <Stack.Navigator>
+                <Stack.Screen name="Main" component={MainTabScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Register" component={Register} />
-                <Stack.Screen name="About Us" component={AboutUs} />
+                <Stack.Screen name="About us" component={AboutUs}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
