7d71e29ebb457075ae69549288a310a9ec039f83
ajvaara <ajvaara@uia.no>
2024-05-13 15:56:34 +0200

    Pin modal & category modal

--- whatsMap/Styles/styles.tsx
+++ whatsMap/Styles/styles.tsx
@@ -168,8 +168,138 @@ const styles = StyleSheet.create({
         },
 });
 
+const category_menu_style = StyleSheet.create({
+    root:
+        {
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 20
+        },
+    modal_overlay:
+        {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    modal_view:
+        {
+            top: 170,
+            width: 250,
+            backgroundColor: BG_COLOUR,
+            borderColor: FG_COLOUR,
+            borderWidth: 1,
+            borderRadius: 20,
+            padding: 10,
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5
+        },
+    option:
+        {
+            padding: 10,
+            marginVertical: 5
+        },
+    button:
+        {
+            backgroundColor: FG_COLOUR,
+            borderWidth: 0,
+            borderRadius: 8,
+            padding: 4,
+            width: 250,
+            alignItems: 'center'
+        },
+    choice_text:
+        {
+            fontSize: 25,
+            fontWeight: "500",
+            color: FG_COLOUR,
+        }
+});
+
+const category_style = StyleSheet.create({
+    root:
+        {
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            paddingVertical: 10,
+            backgroundColor: 'transparent',
+            position: 'absolute',
+            borderWidth:1,
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: 1000,
+        },
+    scroll_view:
+        {
+            paddingHorizontal: 0,
+            marginVertical:-10,
+            height: 50,
+        },
+    button:
+        {
+            backgroundColor: '#ffffff',
+            borderColor: 'black',
+            borderWidth:2,
+            padding: 10,
+            width: 250,
+            alignItems: 'center'
+        },
+    modal_view:
+        {
+            top: 170,
+            width: 250,
+            backgroundColor: "white",
+            borderRadius: 20,
+            padding: 10,
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5
+        },
+    modal_option_box:
+        {
+            paddingVertical: 12, // Increase vertical padding
+            paddingHorizontal: 10,
+            backgroundColor: 'rgba(255,255,255,0.46)', // Very light transparent background
+            borderRadius: 20,
+            marginHorizontal: 5,
+            borderWidth: 1,
+            borderColor: 'rgb(135,206,235)', // Semi-transparent border
+        },
+    modal_option_box_selected:
+        {
+            backgroundColor: 'rgb(135,206,235)', // Visible but transparent
+            borderColor: 'rgba(0, 0, 139, 0.6)', // Noticeably darker border for selected state
+        },
+    modal_text:
+        {
+            fontSize: 14,
+            backgroundColor: 'transparent', // Ensure no background interrupts
+            textAlign: 'center', // Center text if not already set
+        },
+    modal_overlay:
+        {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+});
+
 const pin_style = StyleSheet.create({
-    fullScreenOverlay:
+    fs_overlay:
         {
             flex: 1,
             justifyContent: 'center',
@@ -202,6 +332,8 @@ const pin_style = StyleSheet.create({
         {
             margin: 20,
             backgroundColor: BG_COLOUR,
+            borderColor: FG_COLOUR,
+            borderWidth: 1,
             borderRadius: 20,
             padding: 35,
             alignItems: "center",
@@ -220,9 +352,11 @@ const pin_style = StyleSheet.create({
             height: 40,
             margin: 12,
             borderWidth: 1,
+            borderRadius: 4,
+            borderColor: FG_COLOUR,
             padding: 10,
             width: 200,
-            backgroundColor: 'white',
+            backgroundColor: BG_COLOUR,
             color: FG_COLOUR,
         },
     input_desc:
@@ -230,11 +364,39 @@ const pin_style = StyleSheet.create({
             height: 90,
             margin: 12,
             borderWidth: 1,
+            borderRadius: 4,
+            borderColor: FG_COLOUR,
             padding: 10,
             width: 250,
-            backgroundColor: 'white',
+            backgroundColor: BG_COLOUR,
             color: FG_COLOUR,
+        },
+    button:
+        {
+            backgroundColor: FG_COLOUR,
+            width: 75,
+            height: 35,
+            alignSelf: "center",
+            borderRadius: 4,
+            marginHorizontal: 10,
+        },
+    button_delete:
+        {
+            backgroundColor: RED_COLOUR,
+            width: 75,
+            height: 35,
+            alignSelf: "center",
+            borderRadius: 4,
+            marginHorizontal: 10,
+        },
+    button_text:
+        {
+            color: BG_COLOUR,
+            fontSize: 14,
+            fontWeight: "bold",
+            textAlign: "center",
+            lineHeight: 32,
         },
 });
 
-export {styles, pin_style};
+export {styles, pin_style, category_style, category_menu_style};
